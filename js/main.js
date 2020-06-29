5e3de3a6987298db4e9fca172f581e30f2ca7a66
ANDRESSA <andressajucoski@gmail.com>
2020-06-29 19:23:11 -0300

    commit final

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function() {
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+})
+
+function consultaCep() {
+    $(".barra-progresso").show();
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    console.log(url);
+    $.ajax ({
+        url: url,
+        type: "GET",
+        success: function(response) {
+            console.log(response);
+            $("#titulo_cep").html("CEP " + response.cep);
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $(".cep").show();
+            $(".barra-progresso").hide();
+        }
+    })
+}
